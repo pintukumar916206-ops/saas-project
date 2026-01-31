d073c3e00fc3d8367aedea1daf00a2b27a25fe7e
Pintu Kumar <pintukumar916206@gmail.com>
2026-01-31 14:56:53 +0530

    Daily Update

--- server/controllers/borrowController.js
+++ server/controllers/borrowController.js
@@ -117,7 +117,7 @@ export const borrowBooks = catchAsyncErrors(async (req, res, next) => {
   );
   res.status(200).json({
     success: true,
-    borrowBooks,
+    borrowedBooks: borrowBooks,
   });
 });
 
